43464d7b3474696d8bda68d011eaaa1ae572753c
Brandon <brancarp93@gmail.com>
2021-06-28 17:51:45 -0400

    Up

--- expense.js
+++ expense.js
@@ -4,17 +4,20 @@ const currency = document.querySelector('#payment');
 const item = document.querySelector('#name');
 const theDay = document.querySelector('#date');
 const cost = document.querySelector('#amount');
+const itemLocation = document.querySelector('#location');
 const tBody = document.querySelector('#tBody');
 
 
 
-let newRow = function(theCurrency, theItem, theDate, theCost) {
+let newRow = function(theCurrency, theItem, theLocation, theDate, theCost) {
   let rowElement = document.createElement('tr');
   let currencyElement = document.createElement('td');
   let itemElement = document.createElement('td');
+  let locationElement = document.createElement('td');
   let dateElement = document.createElement('td');
   let costElement = document.createElement('td');
   let delElement = document.createElement('td');
+
 
   if(currency.value === 'card') {
     currencyElement.innerHTML = '<i class="far fa-credit-card"></i>';
@@ -31,10 +34,14 @@ let newRow = function(theCurrency, theItem, theDate, theCost) {
     }   else {
       costElement.innerHTML = '<i class="fas fa-money-bill-wave"></i>'  + cost.value;
     }
-
+  locationElement.textContent = itemLocation.value;
   itemElement.textContent = item.value;
   dateElement.textContent = theDay.value;
   costElement.style = 'color: red';
+
+
+
+
   // Delete Button
   delElement.innerHTML = `<i class="fas fa-trash-alt"></i>`;
   delElement.className = 'delBtn';
@@ -49,6 +56,7 @@ let newRow = function(theCurrency, theItem, theDate, theCost) {
 
   rowElement.appendChild(currencyElement);
   rowElement.appendChild(itemElement);
+  rowElement.appendChild(locationElement);
   rowElement.appendChild(dateElement);
   rowElement.appendChild(costElement);
   rowElement.appendChild(delElement);
@@ -58,6 +66,28 @@ let newRow = function(theCurrency, theItem, theDate, theCost) {
 
 
 
+ 
+  // Delete Button
+  delElement.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+  delElement.className = 'delBtn';
+  
+ delElement.firstChild.addEventListener('click', e => {
+   rowElement.remove();
+ });
+
+
+ if(item.value != '' && cost.value != '' && theDay.value != '') {
+  tBody.appendChild(rowElement);
+
+  rowElement.appendChild(currencyElement);
+  rowElement.appendChild(itemElement);
+  rowElement.appendChild(locationElement);
+  rowElement.appendChild(dateElement);
+  rowElement.appendChild(costElement);
+  rowElement.appendChild(delElement);
+ } else {
+   alert('Fill out inputs');
+ };
 
 }
 
@@ -66,11 +96,14 @@ let newRow = function(theCurrency, theItem, theDate, theCost) {
 document.querySelector('#submitBtn').addEventListener('click', (e) => {
 
   newRow(currency.value, item.value, theDay.value, cost.value);
-  currency.value = '';
-  item.value = '';
-  theDay.value = '';
-  cost.value = '';
+
+
+
+currency.value = '';
+itemLocation.value = '';
+item.value = '';
+theDay.value = '';
+cost.value = '';
 e.preventDefault();
 });
-
 
